e4b2c19055efd1afbff51b5b7116c70153beac03
Alex <mr.requiem.4@gmail.com>
2023-01-25 11:54:34 +0200

    Add docker files

--- resources/js/Components/ApplicationLogoDark.vue
+++ resources/js/Components/ApplicationLogoDark.vue
@@ -1,3 +1,3 @@
 <template>
-    <img src="http://127.0.0.1:8000/assets/images/logo-dark.png" style="max-width:200px"/>
+    <img src="/assets/images/logo-dark.png" style="max-width:200px"/>
 </template>
